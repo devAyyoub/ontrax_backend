032f8bc0fa4efe127cc6022373e81d5c19cb8904
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-09 20:45:20 +0200

    feat: add deleteProject method and route for removing a project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -53,4 +53,20 @@ export class ProjectController {
       console.log(error);
     }
   };
+  static deleteProject = async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id);
+
+      if (!project) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      await project.deleteOne();
+      res.send("Proyecto eliminado");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -43,4 +43,11 @@ router.put(
   ProjectController.updateProject
 );
 
+router.delete(
+    "/:id",
+    param("id").isMongoId().withMessage("ID no válido"),
+    handleInputErrors,
+    ProjectController.deleteProject
+  );
+
 export default router;
