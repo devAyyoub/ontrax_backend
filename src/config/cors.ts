3bf03e69347389f09afb5e7ada7df81d53ede8ae
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-13 01:31:02 +0200

    feat: add CORS support with configuration

---
+++ src/config/cors.ts
@@ -0,0 +1,12 @@
+import cors, { CorsOptions } from "cors";
+
+// Permitir conexiones
+export const corsConfig: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
