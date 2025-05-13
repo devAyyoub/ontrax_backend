cefccebc3995b97ace86e287bdf29bc61a15b77a
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-14 01:45:32 +0200

    feat: add inspector-log and node implementations for debug module

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import cors from "cors";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(cors(corsConfig));
+app.use(morgan('dev'))
 app.use(express.json())
 
 // Routes
