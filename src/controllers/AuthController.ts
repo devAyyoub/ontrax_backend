b7bafbba11e30449a1561dce9ee1fe296f79c1a1
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-16 19:32:41 +0200

    feat: add password hashing utility
    - Implemented `hashPassword` function in `auth.ts` using bcrypt for secure password hashing.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,13 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password } = req.body;
       const user = new User(req.body);
+      user.password = await hashPassword(password);
       await user.save();
       res.send("Cuenta creada, revista tu email para confirmarla");
     } catch (error) {
