0ca357576a2936fbcc93f9e363224b0a803efe51
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-17 18:02:05 +0200

    fix: correct password confirmation field name in account creation validation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -11,7 +11,7 @@ router.post(
   body("password")
     .isLength({ min: 8 })
     .withMessage("La contraseña debe tener mínimo 8 caracteres"),
-  body("password-confirmation").custom((value, { req }) => {
+  body("password_confirmation").custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error("Las contraseñas no coinciden");
     } else {
