ea0fe15d6b98ecfdc6fda351c63e3fe8fb2a6fcb
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-20 01:10:20 +0200

    feat: enhance authentication flow by adding user info to request and extending JWT expiration

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,14 @@
 import { type Request, type Response, type NextFunction, json } from "express";
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User, { IUser } from "../models/User";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
 
 export const authenticate = async (
   req: Request,
@@ -19,8 +27,12 @@ export const authenticate = async (
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (typeof decoded === "object" && decoded.id) {
-      const user = await User.findById(decoded.id);
-      console.log(user);
+      const user = await User.findById(decoded.id).select('id name email');
+      if(user) {
+        req.user = user
+      } else {
+        res.status(500).json({ error: "Token no válido" });
+      }
     }
   } catch (error) {
     res.status(500).json({ error: "Token no válido" });
