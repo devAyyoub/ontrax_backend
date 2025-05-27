4f12ee0f04060662203a609aa6bf628daf924876
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-27 14:30:02 +0200

    feat: add deleteNote endpoint to remove a specific note and handle errors

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import Note, { INote } from "../models/Note";
+import { Types } from "mongoose";
+
+type NoteParams = {
+  noteId: Types.ObjectId;
+};
 
 export class NoteController {
   static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
@@ -19,8 +24,34 @@ export class NoteController {
 
   static getTaskNotes = async (req: Request, res: Response) => {
     try {
-      const notes = await Note.findOne({task: req.task.id})
-      res.json(notes)
+      const notes = await Note.findOne({ task: req.task.id });
+      res.json(notes);
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
+
+  static deleteNote = async (req: Request<NoteParams>, res: Response) => {
+    const { noteId } = req.params;
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      const error = new Error("Nota no encontrada");
+      res.status(404).json({ error: error.message });
+      return;
+    }
+
+    if (note.createdBy.toString() !== req.user.id) {
+      const error = new Error("No tienes permiso para eliminar esta nota");
+      res.status(404).json({ error: error.message });
+      return;
+    }
+
+    req.task.notes = req.task.notes.filter(note => note.toString() !== noteId.toString())
+
+    try {
+        await Promise.all([note.deleteOne(), req.task.save()])
+        res.send('Nota eliminada')
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -145,4 +145,10 @@ router.get('/:projectId/tasks/:taskId/notes',
   NoteController.getTaskNotes
 )
 
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+  param('noteId').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  NoteController.deleteNote
+)
+
 export default router;
