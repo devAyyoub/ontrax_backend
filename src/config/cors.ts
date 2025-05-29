8b95cdb365dfa74917057e53be673896753e05ba
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-29 20:39:59 +0200

    feat: update CORS configuration to allow requests without origin

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,16 +1,20 @@
 import cors, { CorsOptions } from "cors";
 
-// Permitir conexiones
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
     const whitelist = [process.env.FRONTEND_URL];
 
-    if (process.argv[2] === "--api") {
-      whitelist.push(undefined);
-    }
-    if (whitelist.includes(origin)) {
+    // Allow calls without origin (for example: curl, Postman)
+    if (!origin) return callback(null, true);
+
+    const isAllowed = whitelist.some(allowedOrigin =>
+      origin.toLowerCase().startsWith(allowedOrigin?.toLowerCase() || '')
+    );
+
+    if (isAllowed) {
       callback(null, true);
     } else {
+      console.error(`Origin bloqueado por CORS: ${origin}`);
       callback(new Error("Error de CORS"));
     }
   },
