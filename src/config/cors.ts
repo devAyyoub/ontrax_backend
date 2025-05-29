7238fc6a91a5c8e0c69cd68d852fd8dfec172bd4
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-29 21:52:05 +0200

    feat: simplify CORS configuration by removing unnecessary origin checks

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,20 +1,16 @@
 import cors, { CorsOptions } from "cors";
 
+// Permitir conexiones
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
     const whitelist = [process.env.FRONTEND_URL];
 
-    // Allow calls without origin (for example: curl, Postman)
-    if (!origin) return callback(null, true);
-
-    const isAllowed = whitelist.some(allowedOrigin =>
-      origin.toLowerCase().startsWith(allowedOrigin?.toLowerCase() || '')
-    );
-
-    if (isAllowed) {
+    if (process.argv[2] === "--api") {
+      whitelist.push(undefined);
+    }
+    if (whitelist.includes(origin)) {
       callback(null, true);
     } else {
-      console.error(`Origin bloqueado por CORS: ${origin}`);
       callback(new Error("Error de CORS"));
     }
   },
